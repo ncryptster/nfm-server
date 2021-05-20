4956b7dfe35562509a93c85c0bada170fc95eb3b
Erin Ginther <ncryptster@gmail.com>
2021-05-20 09:08:13 -0700

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Web3 = require("web3");
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const mongoose = require("mongoose");
 const Token = require("./models/token");
@@ -46,8 +47,8 @@ router.patch("/:tokenId", async (req, res) => {
 	}
 });
 router.delete('/', async (req, res) => { await Token.deleteMany({}) });
+app.use(cors())
 app.use(express.json());
-app.use(function (req, res, next) { res.header("Access-Control-Allow-Methods", "*"); res.header("Access-Control-Allow-Origin", "*"); res.header( "Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept" ); next(); });
 app.use("/tokens", router);
 app.listen(process.env.PORT || 5000, () => console.log("Server Started"));
 
